e1be858508f15565da13776fc31013ac22ca6fbf
Kamsnyc <kamel.singh1999@gmail.com>
2023-10-24 22:06:32 -0400

    added backend

--- app/components/NoteCard.tsx
+++ app/components/NoteCard.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-function NoteCard() {
+function NoteCard({ note }) {
   return (
     <section className="bg-[#D9E8FC] rounded-2xl">
       {/* CONTENT CONTAINER */}
       <div className="p-4">
         <div>
-          <h1 className="text-[14px] font-black tracking-tight leading-4">Grocery List 10/23/23</h1>
-          <span className="text-[10px] font-medium">To-do list</span>
+          <h1 className="text-[14px] font-black tracking-tight leading-4">{note.title}</h1>
+          <span className="text-[10px] font-medium">{note.category}</span>
         </div>
         {/* LIST */}
-        <ol className="text-[12px] leading-[12px]">
-          <li className='py-1'>Review of Previous Action Items</li>
-          <li className='py-1'>Product Development Update</li>
-          <li className='py-1'>User Feedback and Customer Insights</li>
-          <li className='py-1'>Review of Previous Action Items</li>
-          <li className='py-1'>Roadmap Discussion</li>
-        </ol>
+        <ul className="text-[12px] leading-[12px]">
+          {note.body.map((item, index) => (
+            <li key={index} className='py-1'>{item}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
 }
 
-export default NoteCard
+export default NoteCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,43 @@
-import Image from 'next/image'
-import Logo from './components/Logo'
-import Navbar from './components/Navbar'
-import Filter from './components/Filter'
-import NoteCard from './components/NoteCard'
-import { FaPlus} from 'react-icons/fa'
-import AddButton from './components/AddButton'
+'use client'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NoteCard from './components/NoteCard';
+import Navbar from './components/Navbar';
+import Filter from './components/Filter';
+import AddButton from './components/AddButton';
 
 export default function Home() {
-return (
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/notes');
+      setNotes(response.data);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
     <main className="">
       {/* TOP */}
       <Navbar />
-
-      {/* all notes component */}
       <section className='px-5 relative'>
-         {/* FILTER */}
-         <Filter />
-
-         {/* All Notes */}
-         <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 py-10">
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-            <NoteCard />
-         </div>
-
-         {/* ADD BUTTON */}
-      
-      <AddButton />
+        {/* FILTER */}
+<Filter />        
+{/* All Notes */}
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 py-10">
+          {notes.map((note) => (
+            <NoteCard key={note._id} note={note} />
+          ))}
+        </div>
+        {/* ADD BUTTON */}
+        <AddButton />
       </section>
-
-      
     </main>
-  )
+  );
 }
